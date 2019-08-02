4c10535cad678d4fea24b9169dbaf69a3986abb2
bchutchison <benhutchison85@gmail.com>
2019-08-02 08:41:32 +0100

    output list to page

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  const form = document.querySelector('#new-car-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const make = event.target.make.value;
+    const model = event.target.model.value;
+    const colour = event.target.colour.value;
+    const enginesize = event.target.enginesize.value;
+
+    const formResult = document.createElement('li');
+    formResult.textContent = `${make} ${model} ${colour} ${enginesize}`;
+    const list = document.querySelector('#car-list');
+    list.appendChild(formResult);
+  });
+})
